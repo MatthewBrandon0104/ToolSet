d8b14d7593940e8f6ff82cd4bbc0afa55887cf90
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-07-23 23:29:54 -0700

    second done

--- ruby.rb
+++ ruby.rb
@@ -1,19 +1,27 @@
 module Enumerable
     
-  def my_each
-    new = []
-    i = 0
-    while i < self.length
-        new << yield(self[i])
-        i += 1
+    def my_each
+        new = []
+        i = 0
+        while i < self.length
+            new << yield(self[i])
+            i += 1
+        end
     end
-  p new
-  end
+    
+    def my_each_with_index
+        new = []
+        i = 0
+        while i < self.length
+            new << yield(self[i], i)
+            i += 1
+        end
+    end
 
 
 end
 
 array = [1,2,3,4,5]
-changed = array.my_each { |item| "#{item}" + "x"}
-puts array
-puts changed
+
+array.my_each_with_index { |value, index| puts "index: #{index} of #{value}"}
+array.my_each { |value| puts "#{value}x"}
